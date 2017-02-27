0be6d0209aeac8bb3e9763b1b31acae0a5279a02
yuancihang <yuancihang@aliyun.com>
2017-02-27 16:51:56 +0800

    gzip工具类改用try-with-resources

--- src/main/java/com/happyhttp/util/GzipUtil.java
+++ src/main/java/com/happyhttp/util/GzipUtil.java
@@ -20,16 +20,11 @@ public class GzipUtil {
 	 */
 	public static byte[] compress(byte[] data) throws IOException{
 		ByteArrayOutputStream bos = new ByteArrayOutputStream();
-		GZIPOutputStream gos = null;
-		try {
-			gos = new GZIPOutputStream(bos);
-			
+		try (GZIPOutputStream gos = new GZIPOutputStream(bos)) {
 			gos.write(data);
 			gos.finish();
 			gos.flush();
-		} finally{
-			if(gos != null) gos.close();
-		}
+		} 
 		
 		return bos.toByteArray();
 	}
@@ -42,19 +37,13 @@ public class GzipUtil {
 	 */
 	public static byte[] uncompress(byte[] data) throws IOException {
 		ByteArrayOutputStream bos = new ByteArrayOutputStream();
-		GZIPInputStream gis = null;
-		try {
-			gis = new GZIPInputStream(new ByteArrayInputStream(data));
-
+		try (GZIPInputStream gis = new GZIPInputStream(new ByteArrayInputStream(data))) {
 			byte[] buffer = new byte[8192];
 			int n = 0;
 			while ((n = gis.read(buffer)) >= 0) {
 				bos.write(buffer, 0, n);
 			}
-		} finally {
-			if (gis != null)
-				gis.close();
-		}
+		} 
 
 		return bos.toByteArray();
 	}
